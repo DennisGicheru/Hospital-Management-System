bee66e8d80b02cd0482eacabbfc4789343be76da
DennisGicheru <dennisgicheru698@gmail.com>
2022-09-15 16:18:09 +0300

    basic layout

--- myHMS/myhms/settings.py
+++ myHMS/myhms/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'myhms.wsgi.application'
 DATABASES = {
         'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': 'mydatabase', 
     }
 }
 
